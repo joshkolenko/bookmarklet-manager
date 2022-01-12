c873b9c5b5b892881c8168b59351ebb6ecec2ebf
Josh Kolenko <joshua.kolenko_ic@wwwinc.com>
2022-01-12 14:49:34 -0500

    Add useEffect to override button disabled attribute on mount

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { globalStyles } from '../styled/global';
 import BookmarkletManager from './BookmarkletManager/BookmarkletManager';
 
 export default function App() {
+  const root = document.querySelector('#fed-toolkit');
+
   globalStyles();
+
+  useEffect(() => {
+    root
+      .querySelectorAll('button')
+      .forEach((button) => (button.disabled = false));
+  }, []);
 
   return <BookmarkletManager />;
 }
