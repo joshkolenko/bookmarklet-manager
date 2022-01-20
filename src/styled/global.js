40aa05f8e60fd6c32cda03ecfa7c6c2e239f2b01
Josh Kolenko <joshua.kolenko_ic@wwwinc.com>
2022-01-20 14:37:34 -0500

    Common Tabs component styles

--- src/styled/global.js
+++ src/styled/global.js
@@ -18,4 +18,19 @@ export const globalStyles = globalCss({
   '#fed-toolkit button': {
     cursor: 'pointer',
   },
+
+  '.menu': {
+    '.menu-tabs, .tabs, .scroll-area': {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '.menu-tabs-content, .tabs-content': {
+      height: '100%',
+      '&[data-state="active"]': {
+        display: 'flex',
+        flexDirection: 'column',
+      },
+    },
+  },
 });
