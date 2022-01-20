12307a7199e4e230358154142d69429a0cc93625
Josh Kolenko <joshua.kolenko_ic@wwwinc.com>
2022-01-20 14:37:15 -0500

    Make dark theme default

--- src/styled/styled.js
+++ src/styled/styled.js
@@ -1,12 +1,17 @@
 import { createStitches } from '@stitches/react';
 
-export const { styled, createTheme } = createStitches({
+export const { styled } = createStitches({
   theme: {
     colors: {
-      background: 'white',
-      surface: 'lightgray',
-      text: 'black',
-      border: 'gray',
+      background: '#16181a',
+      surface: '#252729',
+      text: '#7a8188',
+      icon: '#5d6369',
+      title: 'white',
+      hover: 'rgba(128, 139, 150, 0.1)',
+      primary: '#418dfe',
+      primaryA: 'rgba(65, 141, 254, 0.2)',
+      onPrimary: 'white',
     },
     space: {
       1: '6px',
@@ -19,10 +24,11 @@ export const { styled, createTheme } = createStitches({
     },
     fontSizes: {
       1: '13px',
-      2: '15px',
+      2: '16px',
       3: '18px',
       4: '20px',
       5: '24px',
+      6: '30px',
     },
     fontWeights: {
       1: '200',
@@ -35,12 +41,5 @@ export const { styled, createTheme } = createStitches({
       title: ['proxima-nova', 'sans-serif'],
       body: ['yorkten-slab-normal', 'sans-serif'],
     },
-  },
-});
-
-export const darkTheme = createTheme('dark', {
-  colors: {
-    background: 'black',
-    text: 'white',
   },
 });
