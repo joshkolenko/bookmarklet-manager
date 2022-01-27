074ba02c5ff6877268e1c9ab84d737ee9eb1e470
Josh Kolenko <joshua.kolenko_ic@wwwinc.com>
2022-01-27 08:33:39 -0500

    Update Tools.js

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollArea from '../../styled/components/ScrollArea';
 import {
   Tabs,
   TabsContent,
@@ -6,18 +7,22 @@ import {
   TabsList,
   TabsTrigger,
 } from '../../styled/components/Tabs';
+import PageTools from './PageTools';
 
 export default function Tools() {
   return (
-    <Tabs defaultValue="page-tools">
+    <Tabs className="tabs" defaultValue="page-tools">
       <TabsList>
         <TabsTrigger value="page-tools">Page tools</TabsTrigger>
         <TabsTrigger value="site-options">Site options</TabsTrigger>
       </TabsList>
-      <TabsContent value="page-tools">
+      <TabsContent className="tabs-content" value="page-tools">
         <TabsHeader>Page tools</TabsHeader>
+        <ScrollArea>
+          <PageTools />
+        </ScrollArea>
       </TabsContent>
-      <TabsContent value="site-options">
+      <TabsContent className="tabs-content" value="site-options">
         <TabsHeader>Site options</TabsHeader>
       </TabsContent>
     </Tabs>
